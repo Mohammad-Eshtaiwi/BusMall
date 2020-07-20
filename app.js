a164f2e9b3d8c5d9fa40a7ff38792fd12a986d47
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2020-07-20 14:47:38 +0300

    added a chart js and removed pointer from image after the voting end

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 const allProducts = [];
-const arrOfProdects = [];
+const renderedOfProdects = [];
 let temp = [];
 // how many images to dsplay each time
-const numberOfProductsToDisplay = 3;
-let numberOfRounds = 25;
+const numberOfProductsToDisplay = 6;
+let numberOfRounds = 10;
 function Product(name, extention = "jpg") {
   this.name = name;
   this.path = `./img/${name}.${extention}`;
@@ -40,7 +40,7 @@ images.forEach((img) => {
   else allProducts.push(new Product(img.name, img.extention));
 });
 
-function displayRandomProduct() {
+function generateRandomProduct() {
   let count = 0;
   // eslint-disable-next-line no-constant-condition
   while (true) {
@@ -48,41 +48,40 @@ function displayRandomProduct() {
     let randomIndex = randomNum();
     // if the product is not unique then repeat
     if (!allProducts[randomIndex].unique) continue;
-    if (arrOfProdects.includes(allProducts[randomIndex])) continue;
+    if (renderedOfProdects.includes(allProducts[randomIndex])) continue;
     // if all okay then add a product
     allProducts[randomIndex].unique = false;
-    arrOfProdects[count] = allProducts[randomIndex];
+    renderedOfProdects[count] = allProducts[randomIndex];
     // increment the count
     count++;
     // ic count === the number of images that need to be displayed then stop the loop
     if (count === numberOfProductsToDisplay) break;
   }
-  temp = arrOfProdects;
-  console.log("from the random", arrOfProdects);
+  temp = renderedOfProdects;
+  console.log("from the random", renderedOfProdects);
 }
 function displayImages() {
-  displayRandomProduct();
-  console.table(arrOfProdects);
-  console.table(allProducts);
+  generateRandomProduct();
 
   // add one to every shown image
   allProducts.forEach((product) => {
-    for (let index = 0; index < arrOfProdects.length; index++) {
-      if (product === arrOfProdects[index]) product.numberOfShown++;
+    for (let index = 0; index < renderedOfProdects.length; index++) {
+      if (product === renderedOfProdects[index]) product.numberOfShown++;
     }
   });
   const products = document.querySelector(".products");
   products.innerHTML = "";
 
-  for (let product = 0; product < arrOfProdects.length; product++) {
+  for (let product = 0; product < renderedOfProdects.length; product++) {
     // create a figure and add a class product to it
     let figure = document.createElement("figure");
 
     figure.setAttribute("class", "product");
     // create an image
     const img = document.createElement("img");
-    img.setAttribute("src", arrOfProdects[product].path);
-    img.setAttribute("alt", arrOfProdects[product].name);
+    img.setAttribute("src", renderedOfProdects[product].path);
+    img.setAttribute("alt", renderedOfProdects[product].name);
+    img.setAttribute("class", "pointer");
     // add event listener to the imgs
     img.addEventListener("click", votedProducts);
     //append the imag to the fig
@@ -111,14 +110,13 @@ function votedProducts() {
       if (name === temp[i]["name"]) product.unique = false;
     }
   });
-  console.log(arrOfProdects);
-  console.table(allProducts);
   numberOfRounds--;
   if (numberOfRounds === 0) {
-    console.table(allProducts);
     let images = document.querySelectorAll("figure img");
     for (let img = 0; img < images.length; img++) {
       images[img].removeEventListener("click", votedProducts);
+      console.log("removed");
+      images[img].classList.remove("pointer");
     }
     displayResults();
   }
@@ -139,9 +137,63 @@ function displayResults() {
     if (votes > 0) votedProductsList.appendChild(li);
     else unVotedProductsList.appendChild(li);
   });
+  //display the bar graph for the number of showns
+  const numberOfShown = [];
+  const names = [];
+  const colors = [];
+  const borderColors = [];
+  allProducts.forEach((product) => {
+    numberOfShown.push(product.numberOfShown);
+    names.push(product.name);
+    //genarate random rgb colors
+    let tempColor = [randomNum(256), randomNum(256), randomNum(256)];
+    colors.push(`rgba(${tempColor[0]},${tempColor[2]},${tempColor[0]},0.2)`);
+    borderColors.push(
+      `rgba(${tempColor[0]},${tempColor[2]},${tempColor[0]},1)`
+    );
+  });
+  console.log(colors, borderColors);
+  console.log(names, numberOfShown);
+  const graphs = document.querySelector(".graphs");
+  const barCanvas = document.createElement("canvas");
+  const barCanvasCtx = barCanvas.getContext("2d");
+  console.log(barCanvasCtx);
+  console.log(graphs);
+  graphs.appendChild(barCanvas);
+  // eslint-disable-next-line no-undef
+  const numberOfShownGraph = new Chart(barCanvasCtx, {
+    type: "bar",
+    data: {
+      labels: names,
+      datasets: [
+        {
+          label: "# of Shown",
+          data: numberOfShown,
+          backgroundColor: colors,
+          borderColor: borderColors,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+  numberOfShownGraph.canvas.parentNode.style.width = "99%";
+  numberOfShownGraph.canvas.parentNode.style.height = "600px";
 }
 
 //genarate a random number
-function randomNum() {
-  return Math.floor(Math.random() * allProducts.length);
+function randomNum(range = allProducts.length) {
+  return Math.floor(Math.random() * range);
 }
